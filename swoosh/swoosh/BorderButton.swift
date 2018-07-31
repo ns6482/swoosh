c13d17d034d7524b9b534d1c3a80e8819becbea0
Nehal Soni <nehal.soni@britishgas.co.uk>
2018-07-31 18:18:17 +0100

    complete

---
+++ swoosh/swoosh/BorderButton.swift
@@ -0,0 +1,26 @@
+//
+//  BorderButton.swift
+//  swoosh
+//
+//  Created by Nehal Soni on 31/07/2018.
+//  Copyright © 2018 Nehal Soni. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
