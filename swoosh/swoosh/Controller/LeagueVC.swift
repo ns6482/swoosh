cd27965fa5eded453340eb51b546c9b749573434
Nehal Soni <nehalsoni@Nehals-iMac.local>
2018-08-04 23:03:44 +0100

    some event and data handling

--- swoosh/swoosh/Controller/LeagueVC.swift
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
+enum leagueType {
+    case mens
+    case womens
+    case coed
+}
+
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
+        
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegua", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType;
+        nextBtn.isEnabled = true;
+    }
 }
---
+++ swoosh/swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Nehal Soni on 04/08/2018.
+//  Copyright © 2018 Nehal Soni. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
